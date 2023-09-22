14fc14c467b46103024c63fd8d33a9c18c80c134
DavySousa <84909226+DavySousa017@users.noreply.github.com>
2023-09-22 00:26:35 -0300

    Header

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutMe from '@/components/AboutMe'
+import Header from '@/components/Header'
 
 
 const page = () => {
   return (
     <div className='bg-bkgprimary text-white font-custom'>
+      <Header/>
       <AboutMe/>
     </div>
   )
